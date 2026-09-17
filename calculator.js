5e1932d43ee602a218c012c9fcb903a80e7bfa80
zcmckee <zcmckee@aggies.ncat.edu>
2026-09-17 23:45:19 +0000

    calc assignment

--- calculator.js
+++ calculator.js
@@ -26,35 +26,94 @@ function mod(num1, num2){
     return remainder;
 }
 
-function showAlert(){
-    let text = "Do you want to continue? (Yes/No)"
-    alert(text);
+var min;
+var max;
+var avg;
+var total = 0;
+var totalNums = 0;
+let validResults = [];
+
+document.write(`
+    <style>
+
+    }
+    table {
+        width: 60%;
+        margin: 20px 0;
+        font-family: Arial, sans-serif;
+        justify-content: center;
+        border: 1px solid #4CAF50;
+    }
+    th, td {
+        border: 1px solid #333;
+        padding: 8px;
+        text-align: center;
+    }
+    th {
+        background-color: #4CAF50;
+        color: white;
+    }
+    tr:nth-child(even) {
+        background-color: #f2f2f2;
+    }
+    </style>`);
+
+
+document.write("<table>");
+document.write("<tr><th>Number 1</th><th>Operator</th><th>Number 2</th><th>Result</th></tr>")
+while (true){
+
+    let x = prompt("Enter a number: ");
+    if (x === null) break
+
+    let op = prompt("Enter an operator: ")
+    if (op === null) break 
+
+    let y = prompt("Enter another number: ")
+    if (y === null) break
+
+    var numx = Number(x);
+    var numy = Number(y);
+    let result;
+
+    if (isNaN(numx) ||  isNaN(numy)){
+        result =  "Error not a number";
+    } else if (op === "+"){
+        result = add(numx, numy);
+    } else if (op === "-"){
+        result = sub(numx, numy);
+    } else if (op === "*"){
+        result = mult(numx, numy);
+    } else if (op === "/"){
+        result = div(numx, numy);
+    } else if (op === "%"){
+        result = mod(numx, numy);
+    } else {
+        result = "Error: invalid operator";
+    }
+
+    if (Number.isFinite(result)){
+        validResults.push(result)
+        totalNums = totalNums + 2;
+        total = total + result;
+
+    } 
+    
+    
+    document.write("tr><td>" + numx + "</td><td>" + op + "</td><td>" + numy + "</td><td>"+ result + "</td></tr>");
 }
 
+document.write("</table>")
+document.write("<br>");
 
-document.querySelector("form").addEventListener("submit", function(e){
-    e.preventDefault();
-    var x = document.getElementById("x");
-    var y = document.getElementById("y");
-    var op = document.getElementById("op")
+min = Math.min(...validResults);
+max = Math.max(...validResults);
+avg = total / validResults.length;
 
-    var result = document.getElementById("result")
-    var badOp = document.getElementById("badOp");
-    var numx = Number(x.value);
-    var numy = Number(y.value);
+document.write("<table>")
+document.write("<tr><th>Minimum</th><th>Maximum</th><th>Average</th><th>Total</th></tr>")
+document.write("<tr><td>" + min + "</td><td>" + max + "</td><td>" + avg + "</td><td>" + total + "</td></tr>");
+document.write("</table>");
 
-    if (op.value == "+"){
-        result.innerHTML = add(numx, numy);
-    } else if (op.value == "-"){
-        result.innerHTML = sub(numx, numy);
-    } else if (op.value == "*"){
-        result.innerHTML = mult(numx, numy);
-    } else if (op.value == "/"){
-        result.innerHTML = div(numx, numy);
-    } else if (op.value == "%"){
-        result.innerHTML = mod(numx, numy);
-    } else {
-        console.log("Please enter an allowed operator (+, -, *, /, %");
-    };
+document.write("<br><button><a href=index.html>Home</a></button>")
 
-});
